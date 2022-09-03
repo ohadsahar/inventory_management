fab7d9d9be18a82ed11e801d719e765d0f9ab0d8
Ohad Sahar <osahar5@gmail.com>
2022-09-03 13:12:57 +0300

    fixed inventory route

--- core/components/Inventory/Inventory.tsx
+++ core/components/Inventory/Inventory.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import AddItemForm from '../Item/AddItemForm/AddItemForm';
 import ItemsList from '../Item/ItemsList/ItemsList';
 import SearchBar from '../SearchBar/SearchBar';
+import { InventoryWrapper } from './Styled';
 
 const Inventory = () => {
   return (
-    <>
+    <InventoryWrapper>
       <SearchBar />
       <AddItemForm />
       <ItemsList />
-    </>
+    </InventoryWrapper>
   );
 };
 
---
+++ core/components/Inventory/Styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const InventoryWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: calc(100vw - 300px);
+`;
